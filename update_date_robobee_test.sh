d1dffff7aa45c674513953d85386f917b1b924f3
Erwin Müller <erwin.mueller@deventm.org>
2017-05-19 08:10:41 +0200

    Add robobee.

---
+++ update_date_robobee_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export DATE=$(date); export IP_ADDRESS=$(hostname -I|cut -d' ' -f1); ssh -i robobee_id_rsa robobee@robobee-test sudo bash -s <<EOF
+date; echo $DATE; date --set="$DATE"
+sed -ri "s:192.168.178.[0-9]+:$IP_ADDRESS:g" /etc/resolv.conf
+sed -ri 's:192.168.178.[0-9]+:$IP_ADDRESS:g' /etc/dhcp/dhclient.conf
+sed -ri 's:192.168.178.[0-9]+:$IP_ADDRESS:g' /etc/apt/apt.conf.d/01proxy
+EOF
