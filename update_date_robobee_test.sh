76a597f4851f936de94a4609fa15708b774925a3
Erwin Müller <erwin.mueller@deventm.org>
2017-05-29 15:08:37 +0200

    Fix update apt-proxy

--- update_date_robobee_test.sh
+++ update_date_robobee_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DATE=$(date); export IP_ADDRESS=$(hostname -I|cut -d' ' -f1); ssh -i robobee_id_rsa robobee@robobee-test sudo bash -s <<EOF
+export DATE=$(date); export IP_ADDRESS=$(hostname -I|awk '{match($2,"192\\.[0-9]+\\.[0-9]+\\.[0-9]+",a)}END{print a[0]}'); ssh -i robobee_id_rsa robobee@robobee-test sudo bash -s <<EOF
 date; echo $DATE; date --set="$DATE"
-sed -ri 's:192.168.178.[0-9]+:$IP_ADDRESS:g' /etc/apt/apt.conf.d/01proxy
+sed -ri 's:[0-9]+.[0-9]+.[0-9]+.[0-9]+:$IP_ADDRESS:g' /etc/apt/apt.conf.d/80proxy
 EOF
