fd96a52e6df975dde1a699b071eedd8471f9fb7b
Erwin Müller <erwin.mueller@deventm.org>
2017-05-30 13:40:49 +0200

    Use the local user.

--- connect_localhost_test.sh
+++ connect_localhost_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i robobee_id_rsa -o ControlMaster=auto -o ControlPersist=60000 -o ControlPath=/tmp/%r@%h:%p robobee@localhost
+ssh -o ControlMaster=auto -o ControlPersist=60000 -o ControlPath=/tmp/%r@%h:%p localhost
