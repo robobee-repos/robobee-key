953fca2318c0b0a7df8bffdd5617ccd9a9e12109
Erwin Müller <erwin.mueller@deventm.org>
2017-05-31 21:10:07 +0200

    Add option to set IP address.

--- update_date_robobee_test.sh
+++ update_date_robobee_test.sh
@@ -3,7 +3,16 @@ set -x
 
 docker start bind registry apt-cacher-ng
 
-export DATE=$(date); export IP_ADDRESS=$(hostname -I|awk '{match($2,"192\\.[0-9]+\\.[0-9]+\\.[0-9]+",a)}END{print a[0]}'); ssh -i robobee_id_rsa robobee@robobee-test sudo bash -s <<EOF
+if [ $# -eq 1 ]; then
+    IP_ADDRESS=$1; shift
+else
+    IP_ADDRESS=$(hostname -I|awk '{match($2,"192\\.[0-9]+\\.[0-9]+\\.[0-9]+",a)}END{print a[0]}')
+fi
+
+export DATE=$(date)
+export IP_ADDRESS
+
+ssh -i robobee_id_rsa robobee@robobee-test sudo bash -s <<EOF
 date; echo $DATE; date --set="$DATE"
-sed -ri 's:[0-9]+.[0-9]+.[0-9]+.[0-9]+:$IP_ADDRESS:g' /etc/apt/apt.conf.d/80proxy
+sed -ri 's:[0-9]+.[0-9]+.[0-9]+.[0-9]+:$IP_ADDRESS:g' /etc/apt/apt.conf.d/01proxy
 EOF
